1e043b3ff4814f87ae1f43c3ddbb03f2d73dd562
Andrey Zherebor <andreizherebor@yandex.ru>
2024-01-26 23:00:13 +0700

    работа с фото

---
+++ scripts/initialSummer.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/Вишня-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/Гусь-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/Доставка%20роз-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/Земляника-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/Молочник-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/арбузы-min.jpg",
+  },
+  {
+    name: "",
+    link: "https://sprutio.beget.com/image_cache/stone.beget:9443/demaas2q/80130111d1e5a51d91e656f432dee4ca/гриб-min.jpg",
+  },
+];
+
+
