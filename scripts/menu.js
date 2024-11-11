9f6f613eba9d443f57b2b280a261b4cdfb19ba7e
Andrey Zherebor <andreizherebor@yandex.ru>
2024-11-11 19:43:50 +0700

    как давно я этого не далал

---
+++ scripts/menu.js
@@ -0,0 +1,39 @@
+const hamb = document.querySelector("#hamb");
+const popupm = document.querySelector("#popupm");
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popupm.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popupm.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popupm.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
